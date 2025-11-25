0eaa9d8af7e5c8c926c6faa034971519b05e1e71
PhamTuanTu88 <142837726+PhamTuanTu88@users.noreply.github.com>
2025-11-25 13:55:51 +0700

    Update script.js

--- restaurant-order-system/script.js
+++ restaurant-order-system/script.js
@@ -96,6 +96,8 @@ const MENU = {
 { name: "Vịt rang muối", price: 0 },
 { name: "Vịt chiên giềng", price: 0 },
 { name: "Vịt xào hành răm", price: 0 },
+{ name: "Canh miến", price: 0 },
+{ name: "Mì xào", price: 0 },
 { name: "Chả nướng", price: 100000 },
 { name: "Thuốc lá Thăng Long", price: 15000 },
 { name: "Thuốc lá Sài Gòn xanh", price: 15000 },
